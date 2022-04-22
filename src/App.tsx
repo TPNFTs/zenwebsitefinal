635bd0415c1f8014dc722c6864f5b84d3695813d
TPNFTs <tpsoldev@gmail.com>
2022-04-22 17:52:11 +0100

    zensite

---
+++ src/App.tsx
@@ -0,0 +1,97 @@
+import "./App.css";
+import { useMemo } from "react";
+
+import Minter from "./Minter";
+
+import * as anchor from "@project-serum/anchor";
+import { clusterApiUrl } from "@solana/web3.js";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import {
+  getPhantomWallet,
+  getSolflareWallet,
+  getSolletWallet,
+  getMathWallet,
+  getSlopeWallet,
+} from "@solana/wallet-adapter-wallets";
+
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+
+import { WalletDialogProvider } from "@solana/wallet-adapter-material-ui";
+import { ThemeProvider, createTheme } from "@material-ui/core";
+
+const theme = createTheme({
+  palette: {
+    type: "dark",
+  },
+});
+
+const candyMachineId = process.env.REACT_APP_CANDY_MACHINE_ID
+  ? new anchor.web3.PublicKey(process.env.REACT_APP_CANDY_MACHINE_ID)
+  : undefined;
+
+const network = process.env.REACT_APP_SOLANA_NETWORK as WalletAdapterNetwork;
+
+const rpcHost = process.env.REACT_APP_SOLANA_RPC_HOST!;
+const connection = new anchor.web3.Connection(rpcHost);
+
+const startDateSeed = parseInt(process.env.REACT_APP_CANDY_START_DATE!, 10);
+
+const txTimeout = 30000; // milliseconds (confirm this works for your project)
+
+const App = () => {
+  const endpoint = useMemo(() => clusterApiUrl(network), []);
+
+  const wallets = useMemo(
+    () => [
+      getPhantomWallet(),
+      getSolflareWallet(),
+      getSolletWallet(),
+      getMathWallet(),
+      getSlopeWallet(),
+    ],
+    []
+  );
+
+  function toggleMenu() {
+    const menu = document.getElementById("mobileNavContainer")!;
+    menu.classList.toggle("open-menu");
+    console.log("pressed");
+  }
+
+  return (
+    <div>
+      <div className="content-wrapper">
+        <header className="card" id="link1">
+          <div>
+            <ThemeProvider theme={theme}>
+              <ConnectionProvider endpoint={endpoint}>
+                <WalletProvider wallets={wallets} autoConnect>
+                  <WalletDialogProvider>
+                    <Minter
+                      candyMachineId={candyMachineId}
+                      connection={connection}
+                      startDate={startDateSeed}
+                      txTimeout={txTimeout}
+                      rpcHost={rpcHost}
+                    />
+                  </WalletDialogProvider>
+                </WalletProvider>
+              </ConnectionProvider>
+            </ThemeProvider>
+          </div>
+        </header>
+
+        
+
+
+
+        
+      </div>
+    </div>
+  );
+};
+
+export default App;
